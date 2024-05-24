9c83abb81eeff8b9c9fbb74a746a5bf80943f23d
fabryscript <vivaldifabrizio10@gmail.com>
2024-05-24 15:44:10 +0200

    feat: move navbar layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={classNames("bg-neutral-950", inter.className)}>
+      <body
+        className={classNames(
+          "mx-auto max-w-[1440px] bg-neutral-950",
+          inter.className,
+        )}
+      >
+        <div className="flex flex-col gap-1 py-8 text-center text-neutral-200">
+          <h1 className="text-4xl font-bold">
+            Collection of cool web animations
+          </h1>
+          <p className="text-xl">done with Framer Motion + React</p>
+        </div>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,7 @@ const animations: HomeCardProps[] = [
 
 export default function Home() {
   return (
-    <main className="mx-auto flex max-w-[1440px] flex-col gap-16 py-8">
-      <div className="flex flex-col gap-1 text-center text-neutral-200">
-        <h1 className="text-4xl font-bold">
-          Collection of cool web animations
-        </h1>
-        <p className="text-xl">done with Framer Motion + React</p>
-      </div>
+    <main className="flex flex-col gap-16">
       <div className="grid grid-cols-4 gap-4">
         {animations.map((props) => (
           <HomeCard key={props.title} {...props} />
