188a97e54a81d46900b7e386eb10193f1ffa879b
fabryscript <vivaldifabrizio10@gmail.com>
2024-06-03 12:52:55 +0200

    feat(rc): remove unused imports

--- app/(tabs)/rotation-carousel.tsx
+++ app/(tabs)/rotation-carousel.tsx
@@ -1,6 +1,5 @@
 import RotationCarouselComponent from "@/components/rotation-carousel";
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
 
 export default function RotationCarousel() {
   return <RotationCarouselComponent />;
